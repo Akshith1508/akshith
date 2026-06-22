f0d1c72c110095d3023830d49d0a2e7731515b9c
Akshith1508 <akshithnani532@gmail.com>
2026-06-22 16:04:51 +0530

    first commit

--- linkedlist2.py
+++ linkedlist2.py
@@ -5,6 +5,24 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head =None
-        def mid_end(self,data):
-           
+    def mid_end(self,data):
+        new = Node(data)
+        if self.head is None:
+            self.head = new
+            return
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        new.next = slow.next
+        slow.next = new
+    def display(self):
+        itr = self.head
+        while itr:
+            print(itr.data, end=' ')
+            itr = itr.next
+ll = Linkedlist()
+ll.mid_end(50)
+ll.display()
         
